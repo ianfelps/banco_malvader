1b51dc77fbd8de65ed1c068da632a4a0bcd8648b
GUILHERME NEVES MONTANDON FERRAZ <guilherme.ferraz@a.ucb.br>
2024-11-19 10:18:44 -0300

    Arrumado bug

--- src/utils/DBUtils.java
+++ src/utils/DBUtils.java
@@ -12,22 +12,20 @@ import java.util.UUID;
 public class DBUtils {
 
     private static String Sql;
-
     private static String SqlEndereco;
 
     public static String GerarCodigoFunc() {
         return UUID.randomUUID().toString();
     }
 
-
-    public void insertNewUser(int idGerado, String tipoUser, String cargo, Endereco enderecoUsuario){
+    public void insertNewUser(int idGerado, String tipoUser, String cargo, Endereco enderecoUsuario) {
         System.out.println("ID gerado: " + idGerado);
 
-        if(tipoUser.equals("FUNCIONARIO")){
-
+        if (tipoUser.equals("FUNCIONARIO")) {
             String codFunc = GerarCodigoFunc();
 
             setSql("INSERT INTO funcionario (codigo_funcionario, cargo, id_usuario) VALUES (?, ?, ?)");
+            setSqlEndereco("INSERT INTO endereco (cep, local, numero_casa, bairro, cidade, estado, id_usuario) VALUES (?, ?, ?, ?, ?, ?, ?)");
 
             try (Connection conn2 = ConnectionFactory.getConnection();
                  PreparedStatement ps2 = conn2.prepareStatement(Sql);
@@ -36,7 +34,6 @@ public class DBUtils {
                 ps2.setString(1, codFunc);
                 ps2.setString(2, cargo);
                 ps2.setInt(3, idGerado);
-
                 ps2.executeUpdate();
 
                 ps3.setString(1, enderecoUsuario.getCep());
@@ -46,15 +43,14 @@ public class DBUtils {
                 ps3.setString(5, enderecoUsuario.getCidade());
                 ps3.setString(6, enderecoUsuario.getEstado());
                 ps3.setInt(7, idGerado);
-
                 ps3.executeUpdate();
 
             } catch (SQLException e) {
                 e.printStackTrace();
-                throw new RuntimeException("Erro ao inserir cliente na tabela cliente: " + e.getMessage());
+                throw new RuntimeException("Erro ao inserir dados de funcionário ou endereço: " + e.getMessage());
             }
-        }
-        else{
+
+        } else {
             setSql("INSERT INTO cliente (id_usuario) VALUES (?)");
             setSqlEndereco("INSERT INTO endereco (cep, local, numero_casa, bairro, cidade, estado, id_usuario) VALUES (?, ?, ?, ?, ?, ?, ?)");
 
@@ -62,10 +58,11 @@ public class DBUtils {
                  PreparedStatement ps = conn.prepareStatement(Sql);
                  PreparedStatement ps3 = conn.prepareStatement(SqlEndereco)) {
 
+                // Insere dados na tabela 'cliente'
                 ps.setInt(1, idGerado);
-
                 ps.executeUpdate();
 
+                // Insere dados na tabela 'endereco'
                 ps3.setString(1, enderecoUsuario.getCep());
                 ps3.setString(2, enderecoUsuario.getLocal());
                 ps3.setInt(3, enderecoUsuario.getNumeroCasa());
@@ -73,15 +70,13 @@ public class DBUtils {
                 ps3.setString(5, enderecoUsuario.getCidade());
                 ps3.setString(6, enderecoUsuario.getEstado());
                 ps3.setInt(7, idGerado);
-
                 ps3.executeUpdate();
+
             } catch (SQLException e) {
                 e.printStackTrace();
-                throw new RuntimeException("Erro ao inserir cliente na tabela cliente: " + e.getMessage());
+                throw new RuntimeException("Erro ao inserir dados de cliente ou endereço: " + e.getMessage());
             }
         }
-
-
     }
 
     public static String getSqlEndereco() {
