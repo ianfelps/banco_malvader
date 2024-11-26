baf52ed95d41d276ede37de8c0895550b69b0721
Ian Felipe <iannfelps@gmail.com>
2024-11-25 21:14:34 -0300

    update connection factory

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
     // dados para conexao com o banco
     private static final String url = "jdbc:mysql://localhost:3306/banco_malvader";
     private static final String usuario = "root";
-    private static final String senha = "";
+    private static final String senha = "0905";
 
     // metodo para conectar com o banco
     public static Connection getConnection() throws SQLException {
